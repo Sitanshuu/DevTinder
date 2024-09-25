c4bcd1d34f250be30fda37b3b6d7d2075b4f0e5d
Sitanshu Das <sitanshudas2001@gmail.com>
2024-09-26 01:05:29 +0530

    Database Schemas & Models

---
+++ src/3_DatabaseSchemas&Models.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { connectDB } = require("./config/database");
+const User = require("./models/user");
+
+
+
+const PORT = process.env.PORT || 3333;
+const app = express();
+
+app.post("/signup", async (req, res) =>{
+    const userObj = {
+        firstName: "Ananya",
+        lastName: "Das",
+        emailId: "ananyadas2001@gmail.com",
+        password: "Ananya@2001",
+        age: 23,
+        gender: "Female"
+    };
+
+    const user = new User(userObj);     // Creating new instance of a User model..
+    
+    try{      // Always wrap DB Operations inside of try and catch block to handle errors in a good manner
+        await user.save();
+        res.send("User Added Successfully..");
+    }
+    catch(err){
+        res.status(400).send(`Error Saving The User To The Database: ${err.message}`);
+    }
+});
+
+
+
+// First Connect to the database and then listen to incoming requests..
+// If database connection is successful then listen..
+connectDB()
+.then(() =>{
+    console.log("Database Connection Successful...");
+    app.listen(PORT, () =>{
+        console.log(`The application is running on: http://localhost:${PORT}`);
+    });
+})
+.catch((err) =>{
+    console.log(`Error: ${err}`);
+});
