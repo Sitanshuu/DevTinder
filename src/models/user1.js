6946d47b8666f0c65712194522c1e54eaad9475d
Sitanshu Das <sitanshudas2001@gmail.com>
2024-09-30 02:04:34 +0530

    User Schema (Without Validations)

---
+++ src/models/user1.js
@@ -0,0 +1,33 @@
+// Place to define User Schema..
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({     // Pass all the parameter that defines a user..
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    emailId: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    gender: {
+        type: String
+    }
+},
+{
+    timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+
+module.exports = User;
