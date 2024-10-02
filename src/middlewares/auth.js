3fcc951a786c648adc3a3b34d1b2f515ccf31be4
Sitanshu Das <sitanshudas2001@gmail.com>
2024-10-03 01:51:01 +0530

    Authentication Middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,12 +1,45 @@
-const adminAuth = (req, res, next) =>{        // Middleware called for all admin routes & checks for authentication of admin..
-    const token = "admin";
-    const isAdminAuthorized = token === "admin";
-    if (isAdminAuthorized){
+// For Lecture-2
+// const adminAuth = (req, res, next) =>{        // Middleware called for all admin routes & checks for authentication of admin..
+//     const token = "admin";
+//     const isAdminAuthorized = token === "admin";
+//     if (isAdminAuthorized){
+//         next();
+//     }
+//     else{
+//         res.status(401).send("Unauthorized Request!!");
+//     }
+// };
+
+const jwt = require("jsonwebtoken");
+const User = require("../models/user2");
+
+
+// For Lecture-7
+const userAuth = async (req, res, next) =>{
+    try{
+        // 1. Read the token from the req.cookies and validate the token..
+        const { token } = req.cookies;
+        if (!token){
+            throw new Error("Invalid token");
+        }
+        
+        // 2. Verify the token..
+        const decodedObj = await jwt.verify(token, "Dev@Tinder32t276");
+
+        // 3. Find the user
+        const { _id } = decodedObj;
+        const user = await User.findById(_id);
+        if (!user){
+            throw new Error("User not present");
+        };
+        req.user = user;
         next();
     }
-    else{
-        res.status(401).send("Unauthorized Request!!");
+    catch(err){
+        res.status(400).send({ Error: `${err.message}`});
     }
 };
 
-module.exports = {adminAuth};
+
+
+module.exports = { userAuth };
