551183fbcf194e8ed64925a9aacfceb1a1b44f29
Sitanshu Das <sitanshudas2001@gmail.com>
2024-10-07 18:31:26 +0530

    Authentication Route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user2");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
-const { userAuth } = require("../middlewares/auth");
 
 
 const authRouter = express.Router();
