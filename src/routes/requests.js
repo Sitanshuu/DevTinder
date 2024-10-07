2359313f8297850d918195487b142e85bdd8f358
Sitanshu Das <sitanshudas2001@gmail.com>
2024-10-07 18:31:58 +0530

    Request Route

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user2");
 
 
 const requestRouter = express.Router();
@@ -34,6 +36,46 @@ requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) =>{
         res.json({ message: `Connection request sent successfully to ${req.body.to}` });
     }
     catch(err){
+        res.status(400).json({ Error: `${err.message}` });
+    }
+});    // Dummy sendRequest
+
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) =>{
+    try{
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const toUser = await User.findOne({ _id: toUserId });
+        if (!toUser){
+            return res.status(400).json({ Message: "User not found" });
+        };
+        const status = req.params.status;
+
+        const allowedStatus = ["interested", "ignored"];
+        if (!allowedStatus.includes(status)){
+            return res.status(400).json({ Message: `Invalid status type: ${status}`});
+        };
+
+        // If there is an existing connection request..
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            $or: [
+                { fromUserId, toUserId },
+                { fromUserId: toUserId, toUserId: fromUserId }
+            ]
+        });
+        if (existingConnectionRequest){
+            return res.json({ Message: "Connection request already exist" });
+        };
+
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        });
+
+        const data = await connectionRequest.save();
+        res.json({ Message: `Connection request is: ${status}`, data });
+    }
+    catch (err){
         res.status(400).json({ Error: `${err.message}` });
     }
 });
