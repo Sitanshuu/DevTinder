7e509f0bd1befe5e26482f2e9287fe851f083785
Sitanshu Das <sitanshudas2001@gmail.com>
2024-10-23 21:01:00 +0530

    Request Route

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -6,40 +6,6 @@ const User = require("../models/user2");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) =>{
-    try{
-        // const fromUser = req.user;
-        // const { to } = req.body;
-        // const toUser = await User.findOne({ userName: to });
-        // if (!toUser){
-        //     throw new Error("User not found in the database");
-        // }
-
-        // // FROM User Update..
-        // const updatedDataFromUser = {
-        //     requestSent: [{
-        //         to: toUser?.userName,
-        //         status: "pending"
-        //     }]
-        // };
-        // await User.findOneAndUpdate({ userName: fromUser?.userName }, updatedDataFromUser);
-        
-        // // TO User Update..
-        // const updatedDataToUser = {
-        //     requestReceived: [{
-        //         from: fromUser?.userName,
-        //         status: "pending"
-        //     }]
-        // };
-        // await User.findOneAndUpdate({ userName: toUser?.userName }, updatedDataToUser);
-
-        res.json({ message: `Connection request sent successfully to ${req.body.to}` });
-    }
-    catch(err){
-        res.status(400).json({ Error: `${err.message}` });
-    }
-});    // Dummy sendRequest
-
 requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) =>{
     try{
         const fromUserId = req.user._id;
@@ -74,6 +40,42 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
 
         const data = await connectionRequest.save();
         res.json({ Message: `Connection request is: ${status}`, data });
+    }
+    catch (err){
+        res.status(400).json({ Error: `${err.message}` });
+    }
+});
+
+requestRouter.post("/request/review/:status/:requestId", userAuth, async (req, res) =>{
+    try{        
+        //        A  -->  B
+        // 1. Validate the status
+        // 2. Validate the requestId (Is it belonging to B or not)
+        // 3. requestId should be valid and in our database
+        // 4. loggedInUserId === toUserId
+        // 5. Status must be interested
+        
+        const loggedInUser = req.user;
+        const status = req.params.status;
+        const requestId = req.params.requestId;
+
+        const allowedStatus = ["accepted", "rejected"];
+        if (!allowedStatus.includes(status)){
+            return res.status(400).json({ Message: `Invalid status type: ${status}` });
+        }
+
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInUser._id,
+            status: "interested"
+        });
+        if (!connectionRequest){
+            return res.status(400).json({ Message: "No connection request found" });
+        }
+
+        connectionRequest.status = status;
+        const data = await connectionRequest.save();
+        res.json({ Message: data });
     }
     catch (err){
         res.status(400).json({ Error: `${err.message}` });
